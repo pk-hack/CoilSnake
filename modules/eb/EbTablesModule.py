82dd92b8c31d9e3b795feea8301a11bc660a401a
MrTenda <arewereally@gmail.com>
2012-04-21 02:38:15 -0400

    Added error message for invalid labels.

--- modules/eb/EbTablesModule.py
+++ modules/eb/EbTablesModule.py
@@ -13,7 +13,8 @@ class PointerTableEntry(IntTableEntry):
                 self._data = EbModule.labelsDict[data]
             except KeyError:
                 # TODO Error, invalid label
-                self._data = 0
+                # self._data = 0
+                raise RuntimeError("Invalid label: '" + data + "'")
     def dump(self):
         return '$' + hex(self._data)[2:]
 
