454b8a467fb7bd5c75ef8d7752cacc005bb4a95f
MrTenda <arewereally@gmail.com>
2012-04-28 03:36:53 -0400

    Disabled ExpTablesModule due to bugs

--- modules/eb/MiscTablesModule.py
+++ modules/eb/MiscTablesModule.py
@@ -1,21 +1,21 @@
 import EbTablesModule
 
 class MiscTablesModule(EbTablesModule.EbTablesModule):
-    _name = "Standard Tables"
+    _name = "Misc Tables"
     _tableIDs = [
             0xC3FD8D, # Attract mode text
             0xD5F645, # Timed Item Delivery
 #            0xE01FC8, # Text Window Flavor palettes
             0xE12F8A, # Photographer
             0xEFA37A, # Command Window text?
-#            0xCF8985, # NPC Configuration Table
+            0xCF8985, # NPC Configuration Table
             0xD5EA77, # Condiment Table
-#            0xD5EBAB, # Scripted Teleport Destination Table
-#            0xD5F2FB, # Hotspots Table
+            0xD5EBAB, # Scripted Teleport Destination Table
+            0xD5F2FB, # Hotspots Table
             0xC3F2B5, # Playable Character Graphics Control Table
-#            0xD58D7A, # PSI Names
-#            0xD58A50, # PSI Abilities
-#            0xD57B68, # Battle Actions Table
+            0xD58D7A, # PSI Names
+            0xD58A50, # PSI Abilities
+            0xD57B68, # Battle Actions Table
             0xD5EA5B, # Statistic Growth Variables
             0xD58F49, # Level-up EXP Table
             0xD5F5F5, # Initial Stats Table
@@ -24,9 +24,9 @@ class MiscTablesModule(EbTablesModule.EbTablesModule):
             0xD576B2, # Store Inventory Table
             0xD5F4BB, # Timed Item Transformations
             0xD5F4CF, # Don't Care
-#            0xD55000, # Item Data
+            0xD55000, # Item Data
             0xC23109, # Consolation Item
-#            0xC3E250, # Windows
+            0xC3E250, # Windows
 #            0xC3F054, # Font Ptr Tbl
 #            0xC4C05E, # File select text # TODO need to fix this
 #            0xC8CDED, # Compressed text ptr tbl
