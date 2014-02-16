64b0baeefa51657e8a208a394334fb77465e1eb6
kiij <arewereally@gmail.com>
2014-02-15 22:22:31 -0800

    Clearing all __init__.py files

--- coilsnake/modules/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["GenericModule", "TablesModule", "RawBlocksModule"]
--- coilsnake/modules/eb/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = [
-    "EbModule",
-    "MapModule",
-    "EbTablesModule",
-    "EbBlocksModule",
-    "EnemyModule"]
