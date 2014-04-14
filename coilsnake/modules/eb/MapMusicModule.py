d7ad882ce849fb3b54aca57edf9e429e21c652a6
kiij <arewereally@gmail.com>
2014-04-14 01:42:00 -0700

    BUGFIX: Write correct pointer to map music data

--- coilsnake/modules/eb/MapMusicModule.py
+++ coilsnake/modules/eb/MapMusicModule.py
@@ -5,7 +5,7 @@ from coilsnake.model.eb.table import eb_table_from_offset, EbBankPointerToVariab
     EbPointerTableEntry
 from coilsnake.modules.eb import EbModule
 from coilsnake.util.eb.helper import not_in_bank
-from coilsnake.util.eb.pointer import from_snes_address
+from coilsnake.util.eb.pointer import from_snes_address, to_snes_address
 
 
 MAP_MUSIC_ASM_POINTER_OFFSET = 0x6939
@@ -25,7 +25,6 @@ class MapMusicModule(EbModule.EbModule):
                 0x0f))
 
     def read_from_rom(self, rom):
-        print hex(from_snes_address(rom.read_multi(MAP_MUSIC_ASM_POINTER_OFFSET, 3)))
         self.pointer_table.from_block(rom, from_snes_address(rom.read_multi(MAP_MUSIC_ASM_POINTER_OFFSET, 3)))
 
     def write_to_rom(self, rom):
@@ -33,7 +32,7 @@ class MapMusicModule(EbModule.EbModule):
 
         pointer_table_offset = rom.allocate(size=self.pointer_table.size, can_write_to=partial(not_in_bank, 0x0f))
         self.pointer_table.to_block(rom, pointer_table_offset)
-        rom.write_multi(MAP_MUSIC_ASM_POINTER_OFFSET, pointer_table_offset, 3)
+        rom.write_multi(MAP_MUSIC_ASM_POINTER_OFFSET, to_snes_address(pointer_table_offset), 3)
 
     def write_to_project(self, resource_open):
         with resource_open("map_music", "yml") as f:
