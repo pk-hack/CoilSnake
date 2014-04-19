394fd97ad28cf66bd0eecb19ccfa9ae76d31dd2e
kiij <arewereally@gmail.com>
2014-04-19 14:09:46 -0700

    Minor clean up UsedRangeModule

--- coilsnake/modules/common/UsedRangeModule.py
+++ coilsnake/modules/common/UsedRangeModule.py
@@ -1,6 +1,5 @@
 import yaml
 
-from coilsnake.Progress import updateProgress
 from coilsnake.modules.common.GenericModule import GenericModule
 
 MODULE_COMMENT = """# List all ranges which CoilSnake should not touch
@@ -19,24 +18,22 @@ class UsedRangeModule(GenericModule):
         GenericModule.__init__(self)
         self.ranges = []
 
-    def upgrade_project(self, old_version, new_version, rom,
-            resource_opener_r, resource_opener_w, resource_deleter):
+    def upgrade_project(self, old_version, new_version, rom, resource_open_r, resource_open_w, resource_delete):
         """
             Upgrades a project's used ranges module to the latest version.
         """
         if old_version == new_version:
-            updateProgress(100)
             return
-        if oldVersion == 3:
+        if old_version == 3:
             self.read_from_rom(rom, False)
-            self.write_to_project(resource_opener_w, False)
+            self.write_to_project(resource_open_w, False)
         self.upgrade_project(
             old_version + 1,
             new_version,
             rom,
-            resource_opener_r,
-            resource_opener_w,
-            resource_deleter)
+            resource_open_r,
+            resource_open_w,
+            resource_delete)
 
     def write_to_project(self, resource_opener):
         """
@@ -44,7 +41,6 @@ class UsedRangeModule(GenericModule):
         """
         with resource_opener(self.FILE, 'yml') as f:
             f.write(MODULE_COMMENT)
-        updateProgress(50)
 
     def read_from_project(self, resourceOpener):
         """
@@ -55,11 +51,7 @@ class UsedRangeModule(GenericModule):
             if not ranges:
                 self.ranges = []
             else:
-                self.ranges = map(
-                    lambda y: tuple(map(lambda z: int(z, 0),
-                                        y[1:-1].split(','))),
-                    ranges)
-        updateProgress(50)
+                self.ranges = [tuple([int(z, 0) for z in y[1:-1].split(',')]) for y in ranges]
 
     def read_from_rom(self, rom, u=True):
         """
@@ -67,8 +59,6 @@ class UsedRangeModule(GenericModule):
             user-specified.
         """
         self.ranges = []
-        if u:
-            updateProgress(50)
 
     def write_to_rom(self, rom, u=True):
         """
@@ -77,5 +67,3 @@ class UsedRangeModule(GenericModule):
         """
         for r in self.ranges:
             rom.mark_allocated(r)
-        if u:
-            updateProgress(50)
