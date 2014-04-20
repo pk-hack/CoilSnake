19a925992840180a93eac344bf8828b87fe86286
kiij <arewereally@gmail.com>
2014-04-20 15:00:16 -0700

    Remove unused EB0 stuff

--- coilsnake/modules/eb0/Eb0Module.py
+++
@@ -1,6 +0,0 @@
-from coilsnake.modules.common.GenericModule import GenericModule
-
-
-class Eb0Module(GenericModule):
-    def compatibilityWithRomtype(self, romtype):
-        return romtype == "Earthbound Zero"
