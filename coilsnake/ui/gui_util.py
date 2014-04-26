aa98c71c1528a57c0c5b8d7b935bae693bad1c5c
kiij <arewereally@gmail.com>
2014-04-26 00:57:59 -0700

    Useful initial directories for file select dialogs

--- coilsnake/ui/gui_util.py
+++ coilsnake/ui/gui_util.py
@@ -1,4 +1,5 @@
 from Tkconstants import END
+import os
 import subprocess
 import tkFileDialog
 import tkMessageBox
@@ -80,11 +81,15 @@ def set_entry_text(entry, text):
 def browse_for_rom(root, entry, save=False):
     if save:
         filename = tkFileDialog.asksaveasfilename(
-            parent=root, title="Select an output ROM",
+            parent=root,
+            initialdir=os.path.dirname(entry.get()),
+            title="Select an output ROM",
             filetypes=[('SNES ROMs', '*.smc'), ('SNES ROMs', '*.sfc'), ('All files', '*.*')])
     else:
         filename = tkFileDialog.askopenfilename(
-            parent=root, title="Select a ROM",
+            parent=root,
+            initialdir=os.path.dirname(entry.get()),
+            title="Select a ROM",
             filetypes=[('SNES ROMs', '*.smc'), ('SNES ROMs', '*.sfc'), ('All files', '*.*')])
     if filename:
         set_entry_text(entry, filename)
@@ -94,6 +99,7 @@ def browse_for_rom(root, entry, save=False):
 def browse_for_project(root, entry, save=False):
     filename = tkFileDialog.askdirectory(
         parent=root,
+        initialdir=os.path.dirname(entry.get()),
         title="Select a Project Directory",
         mustexist=(not save))
     if filename:
