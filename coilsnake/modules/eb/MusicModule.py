53a6c47132fb9612f34c6e6f272a71bf5fd8d753
kiij <kiijmp@gmail.com>
2014-05-17 20:27:29 -0700

    Music - decompiling sequences which are embedded in the "program" chunk

--- coilsnake/modules/eb/MusicModule.py
+++ coilsnake/modules/eb/MusicModule.py
@@ -23,6 +23,7 @@ class MusicModule(EbModule):
         super(MusicModule, self).__init__()
         self.pack_pointer_table = eb_table_from_offset(offset=PACK_POINTER_TABLE_OFFSET)
         self.music_dataset_table = eb_table_from_offset(offset=MUSIC_DATASET_TABLE_OFFSET)
+        self.instrument_sets = dict()
         self.sequences = []
 
     def read_from_rom(self, rom):
@@ -34,7 +35,18 @@ class MusicModule(EbModule):
         pack_offsets = [from_snes_address(self.pack_pointer_table[i][0])
                         for i in range(self.pack_pointer_table.num_rows)]
         packs = [read_pack(rom, x) for x in pack_offsets]
-        program = packs[1][0x0500].data
+        program_chunk = packs[1][0x0500]
+
+        # Read the instrument sets
+        instrument_pack_ids = reduce(lambda x, y: x.union(y),
+                                     [[self.music_dataset_table[bgm_id][0], self.music_dataset_table[bgm_id][1]]
+                                         for bgm_id in range(self.music_dataset_table.num_rows)],
+                                     set())
+        for instrument_pack_id in instrument_pack_ids:
+            if instrument_pack_id == 0xff:
+                continue
+            pack = packs[instrument_pack_id]
+            #self.instrument_sets[instrument_pack_id] =
 
         # Read the sequences
         for bgm_id in range(self.music_dataset_table.num_rows):
@@ -44,7 +56,9 @@ class MusicModule(EbModule):
                 sequence_pack = None
             else:
                 sequence_pack = packs[sequence_pack_id]
-            sequence_chunk = get_sequence_as_chunk(bgm_id=bgm_id, program=program, sequence_pack=sequence_pack)
+            sequence_chunk = get_sequence_as_chunk(bgm_id=bgm_id,
+                                                   program_chunk=program_chunk,
+                                                   sequence_pack=sequence_pack)
             self.sequences[bgm_id] = sequence_chunk
 
     def write_to_project(self, resource_open):
