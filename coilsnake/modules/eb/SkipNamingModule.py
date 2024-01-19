b8b3a9ea8684b401d708983daab4b2838ba44d5b
Cooper Harasyn <charasyn@outlook.com>
2024-01-19 13:22:38 -0800

    Add fixes for new Coilsnake features overwriting CCS patch code

--- coilsnake/modules/eb/SkipNamingModule.py
+++ coilsnake/modules/eb/SkipNamingModule.py
@@ -1,8 +1,11 @@
+import logging
 from coilsnake.modules.eb.EbModule import EbModule
 from coilsnake.util.common.yml import yml_load, yml_dump
 from coilsnake.util.eb.pointer import to_snes_address
 from coilsnake.util.eb.text import standard_text_to_byte_list
+from coilsnake.exceptions.common.exceptions import CoilSnakeUserError
 
+log = logging.getLogger(__name__)
 
 class SkipNamingModule(EbModule):
     NAME = "Skip Names"
@@ -35,14 +38,20 @@ class SkipNamingModule(EbModule):
 
     def write_to_rom(self, rom):
         if self.data["Enable Skip"]:
-            rom[0x1faae] = 0x5c
-
             # this fixes the naming screen music playing briefly when skip naming is on
             # it works by changing the jump from @CHANGE_TO_NAMING_SCREEN_MUSIC to @UNKNOWN18 (which normally runs directly after the music change)
             # https://github.com/Herringway/ebsrc/blob/87f514cb4b77fa3193bcb122ea51f5de5cfdd9cf/src/intro/file_select_menu_loop.asm#L101
-            rom[0x1f8f1] = 0x10
+            # Verify the code structure first:
+            if rom[0x1f8f0] == 0xD0 and rom[0x1f8f1] == 0x09:
+                rom[0x1f8f1] = 0x10
+            else:
+                log.warn("Unable to apply naming screen music bypass due to existing ASM changes")
 
             offset = rom.allocate(size=(10 + 4 * 5 * 5 + 3 * 6 * 5))
+            # Patch ASM to "JML newCode"
+            if bytes(rom.to_array()[0x1faae:0x1fab2]) != b'\xa9\x07\x00\x18':
+                raise CoilSnakeUserError("Naming ASM has already been patched - unable to apply naming skip")
+            rom[0x1faae] = 0x5c
             rom.write_multi(0x1faaf, to_snes_address(offset), 3)
             rom[offset:offset+4] = [0x48, 0x08, 0xe2, 0x20]
             offset += 4
