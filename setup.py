c3b595050311093747d95770017d1f284a0ebab4
PhoenixBound <phoenixbound.github@gmail.com>
2024-08-21 17:26:19 -0500

    Modernize build methods and instructions
    So far, this works well enough to install the package "successfully." I
    haven't tested whether it actually works or not, or whether the git
    version number integration works properly, or whether you can make an
    EXE. (I used Python 3.10.6 on Windows 10 for testing so far. I know of
    issues with the spec file.)
    
    While I was here, I tried moving everything I could out of setup.py and
    into the new file format shared by all build systems. Maybe that was a
    bit unnecessary... But I figure pyproject.toml is easier to analyze with
    tools, for people who want to scour GitHub and identify security
    problems in popular packages and stuff like that.

--- setup.py
+++ setup.py
@@ -1,52 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import os
 import platform
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.extension import Extension
 
 extra_compile_args = []
 
 if platform.system() != "Windows":
-    extra_compile_args = ["-std=c99"]
-
-install_requires = [
-    "Pillow>=3.0.0",
-    "PyYAML>=3.11",
-    "CCScriptWriter>=1.2",
-    "ccscript>=1.500"
-]
-
-if platform.system() == "Darwin":
-    install_requires.append("pyobjc-framework-Cocoa")
+    extra_compile_args.append("-std=c99")
 
 setup(
-    name="coilsnake",
-    version="4.2",
-    description="CoilSnake",
-    url="https://pk-hack.github.io/CoilSnake",
-    packages=find_packages(),
-    include_package_data=True,
-
-    install_requires=install_requires,
-    dependency_links=[
-        "https://github.com/Lyrositor/CCScriptWriter/tarball/master#egg=CCScriptWriter-1.2",
-        "https://github.com/charasyn/ccscript_legacy/archive/refs/tags/v1.500.tar.gz#egg=ccscript-1.500"
-    ],
     ext_modules=[
         Extension(
             "coilsnake.util.eb.native_comp",
             ["coilsnake/util/eb/native_comp.c", "coilsnake/util/eb/exhal/compress.c"],
+            # include_dirs=['coilsnake/util/eb/exhal'],
             extra_compile_args=extra_compile_args,
         )
     ],
-    entry_points={
-        "console_scripts": [
-            "coilsnake = coilsnake.ui.gui:main",
-            "coilsnake-cli = coilsnake.ui.cli:main"
-        ]
-    },
-
     test_suite="nose.collector",
     tests_require=[
         "nose>=1.0",
